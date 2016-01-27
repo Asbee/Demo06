5c3cee1dc0c91d31e725cb35408c72d4e4a617be
Asbee <Santtu_boman@hotmail.com>
2016-01-27 15:28:19 +0200

    KT3

---
+++ KT3/Suits.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KT3
+{
+    class Suits
+    {
+       enum Suit
+        {
+            Diamonds,
+            Hearts,
+            Clubs,
+            Spades,
+        }
+
+    }
+}
